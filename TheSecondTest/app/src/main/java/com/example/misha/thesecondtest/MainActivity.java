ccf0732a92c6d7122301c8d9d8e9b0075eaae5c7
Misha <misha_t@ua.fm>
2015-08-24 00:11:38 +0300

    Calculator is done with bugs for now

--- TheSecondTest/app/src/main/java/com/example/misha/thesecondtest/MainActivity.java
+++ TheSecondTest/app/src/main/java/com/example/misha/thesecondtest/MainActivity.java
@@ -10,14 +10,18 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    private TextView mPawnText;
-    private  int mCount = 0;
+    private TextView mInputText;
+    private TextView mStatusBar;
+    private boolean isNewValue = false;
+    private float mCurrentValue = 0;
+    private String mOperator = ""; //todo: change it to the special type
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPawnText = (TextView)findViewById(R.id.textView2);
+        mInputText = (TextView)findViewById(R.id.inputText);
+        mStatusBar = (TextView)findViewById(R.id.textViewOperator);
     }
 
     @Override
@@ -42,14 +46,137 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    public void onPawnClick(View view) {
-        mPawnText.setText("Hello Pawn");
+//==================================================
+    public void setOperator(String newOperator) {
+        mStatusBar.setText(newOperator);
+        doCalculate();
+        mOperator = newOperator;
     }
 
-    public void onCrowsCounterClick(View view) {
-        TextView lTextViewHello = (TextView)findViewById(R.id.textViewHello);
-       // mCount = mCount + 1;
-        lTextViewHello.setText("I counted " + ++mCount + " Crowns");
+    public void setInputText(String digit) {
+        String lCurrentText = mInputText.getText().toString();
+
+        if ((isNewValue) || (Float.parseFloat(lCurrentText) == 0)){
+            mInputText.setText(digit);
+            isNewValue = false;
+        }
+        else {
+            mInputText.setText(lCurrentText + digit);
+
+        }
+    }
+
+    public void doCalculate(){
+        float lEditingValue = Float.parseFloat(mInputText.getText().toString());
+        float lResult = 0;
+       // if (isNewValue)
+         //   return;
+
+        if ((mCurrentValue == 0)||(isNewValue)){
+            mCurrentValue = lEditingValue;
+           }
+        else {
+            switch (mOperator){
+                case "+": {
+                    lResult = mCurrentValue + lEditingValue;
+                }
+                break;
+                case "-": {
+                    lResult = mCurrentValue - lEditingValue;
+                }
+                break;
+                case "/": {
+                    if (lEditingValue != 0) {
+                        lResult = mCurrentValue / lEditingValue;
+                    }
+                }
+                break;
+                case "*": {
+                    lResult = mCurrentValue * lEditingValue;
+                }
+                break;
+
+            }
+
+            mInputText.setText(Float.toString(lResult));
+            mCurrentValue = 0;
+        }
+        isNewValue = true;
+    }
+
+    public void btnClearClick(View view) {
+        mInputText.setText("0");
+        mCurrentValue = 0;
+        mOperator = "";
+    }
+
+    public void btnEqualClick(View view) {
+        setOperator("=");
+        //do calculate result here
+    }
+    //-----------------------------------------
+    public void btnPlusClick(View view) {
+        setOperator("+");
+    }
+
+    public void btnMinusClick(View view) {
+        setOperator("-");
+    }
+
+    public void btnSignClick(View view) {
+        String lText = mInputText.getText().toString();
+        if (Float.parseFloat(lText) == 0)
+            return;
+
+        if (lText.substring(0, 1).equals("-")) {
+            lText = lText.substring(1, lText.length());
+        }
+        else {
+            lText = "-" + lText;
+        }
+        mInputText.setText(lText);
+
+    }
+
+    public void btnDivideClick(View view) {
+        setOperator("/");
+    }
+
+    public void btnMultiplyClick(View view) {
+        setOperator("*");
+    }
+//-----------------------------------------
+    public void btn1Click(View view) {
+        setInputText("1");
+    }
+    public void btn2Click(View view) {
+        setInputText("2");
+    }
+    public void btn3Click(View view) {
+        setInputText("3");
+    }
+    public void btn4Click(View view) {
+        setInputText("4");
+    }
+    public void btn5Click(View view) {
+        setInputText("5");
+    }
+    public void btn6Click(View view) {
+        setInputText("6");
+    }
+    public void btn7Click(View view) {
+        setInputText("7");
+    }
+    public void btn8Click(View view) {
+        setInputText("8");
+    }
+    public void btn9Click(View view) {
+        setInputText("9");
+    }
+    public void btn0Click(View view) {
+        setInputText("0");
+    }
+    public void btnDotClick(View view) {
+        setInputText(".");
     }
 }
