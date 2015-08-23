2c57a3aeaf86c8ec01ccee8e3fb1713232aa12c5
Misha <misha_t@ua.fm>
2015-08-23 18:52:39 +0300

    added button

--- TheSecondTest/app/src/main/java/com/example/misha/thesecondtest/MainActivity.java
+++ TheSecondTest/app/src/main/java/com/example/misha/thesecondtest/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView mPawnText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPawnText = (TextView)findViewById(R.id.textView2);
     }
 
     @Override
@@ -34,5 +39,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onPawnClick(View view) {
+        mPawnText.setText("Hello Pawn");
     }
 }
