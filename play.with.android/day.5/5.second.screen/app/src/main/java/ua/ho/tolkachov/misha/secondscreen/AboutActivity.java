6bca562c1d75c9efe0d0d32fdb5528b340ededec
Misha <misha_t@ua.fm>
2016-01-13 23:37:00 +0200

    second screen can appear

---
+++ play.with.android/day.5/5.second.screen/app/src/main/java/ua/ho/tolkachov/misha/secondscreen/AboutActivity.java
@@ -0,0 +1,15 @@
+package ua.ho.tolkachov.misha.secondscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Misha on 13.01.2016.
+ */
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- play.with.android/day.5/5.second.screen/app/src/main/java/ua/ho/tolkachov/misha/secondscreen/MainActivity.java
+++ play.with.android/day.5/5.second.screen/app/src/main/java/ua/ho/tolkachov/misha/secondscreen/MainActivity.java
@@ -1,9 +1,12 @@
 package ua.ho.tolkachov.misha.secondscreen;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -33,5 +36,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
     }
 }
