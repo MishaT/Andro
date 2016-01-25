92c58ee6fd3df9b60b15ae59d5886c09f4b1cd46
Misha <misha_t@ua.fm>
2016-01-26 01:54:25 +0200

    applied theme for about dialog

---
+++ play.with.android/day.7/7.ThemesAndStyles/app/src/main/java/ua/ho/tolkachov/misha/themesandstyles/AboutActivity.java
@@ -0,0 +1,16 @@
+package ua.ho.tolkachov.misha.themesandstyles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Misha on 26.01.2016.
+ */
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+    }
+}
