f31665a5c1ed5edf58cc4eb5c855d4b77541f476
Misha <misha_t@ua.fm>
2016-01-26 01:35:50 +0200

    init version for styles

---
+++ play.with.android/day.7/7.ThemesAndStyles/app/src/test/java/ua/ho/tolkachov/misha/themesandstyles/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ua.ho.tolkachov.misha.themesandstyles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
