4b8099e983469f1ea7401947c0623b352a7f992d
Misha <misha_t@ua.fm>
2016-02-12 23:37:21 +0200

    created menu

--- play.with.android/day.10/10.Menu/app/src/main/java/ua/ho/tolkachov/misha/menu/MainActivity.java
+++ play.with.android/day.10/10.Menu/app/src/main/java/ua/ho/tolkachov/misha/menu/MainActivity.java
@@ -2,6 +2,10 @@ package ua.ho.tolkachov.misha.menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        TextView infoTextView = (TextView)findViewById(R.id.TextView);
+        switch (itemId) {
+            case R.id.action_settings:
+                infoTextView.setText("you selected SETTINGS");
+                return true;
+            case R.id.action_man:
+                infoTextView.setText("you selected MAN");
+                return true;
+            case R.id.action_femail:
+                infoTextView.setText("you selected WOMAN");
+                return true;
+            case R.id.action_child:
+                infoTextView.setText("you selected KID");
+                Toast.makeText(MainActivity.this, "you selected KID", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
