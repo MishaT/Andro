c65e0d964c1a4cce10828a481449e7f1a0bf195b
Misha <misha_t@ua.fm>
2016-02-22 03:53:20 +0200

    created an empty menu

--- play.with.android/day.11/11.PopupMenu/app/src/main/java/ua/ho/tolkachov/misha/popupmenu/MainActivity.java
+++ play.with.android/day.11/11.PopupMenu/app/src/main/java/ua/ho/tolkachov/misha/popupmenu/MainActivity.java
@@ -2,6 +2,11 @@ package ua.ho.tolkachov.misha.popupmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button1);
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+
+        button.setOnClickListener(viewOnClickListener);
+        textView.setOnClickListener(viewOnClickListener);
+        imageView.setOnClickListener(viewOnClickListener);
+    }
+
+    View.OnClickListener viewOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            showPopupMenu(v);
+        }
+    };
+
+    private void showPopupMenu(View v){
+        Toast.makeText(getApplicationContext(), "POPUP", Toast.LENGTH_LONG).show();
+
+
     }
 }
