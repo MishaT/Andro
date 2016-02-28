ed4d017f4edcebc8347bf0d67da1fb91453c8ec4
Misha <misha_t@ua.fm>
2016-02-28 14:31:29 +0200

    finished day 12

--- play.with.android/day.12/12.NavigationDrawer/app/src/main/java/ua/ho/tolkachov/misha/navigationdrawer/MainActivity.java
+++ play.with.android/day.12/12.NavigationDrawer/app/src/main/java/ua/ho/tolkachov/misha/navigationdrawer/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        drawer.openDrawer(GravityCompat.START);
     }
 
     @Override
@@ -81,7 +83,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Toast.makeText(getApplicationContext(), "You selected Camera", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
