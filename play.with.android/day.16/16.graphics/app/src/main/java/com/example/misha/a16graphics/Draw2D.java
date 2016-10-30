2e075a9f5b515b0ad79cdd83f607730aa91ffd9c
Misha <misha_t@ua.fm>
2016-10-30 20:58:00 +0200

    lion the king, graphics

--- play.with.android/day.16/16.graphics/app/src/main/java/com/example/misha/a16graphics/Draw2D.java
+++ play.with.android/day.16/16.graphics/app/src/main/java/com/example/misha/a16graphics/Draw2D.java
@@ -1,9 +1,13 @@
 package com.example.misha.a16graphics;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -13,9 +17,15 @@ import android.view.View;
 public class Draw2D extends View {
     public Draw2D(Context context){
         super(context);
+
+        //get image from resources
+        Resources res = this.getResources();
+        mBitmap = BitmapFactory.decodeResource(res, R.drawable.lion_king);
     }
 
     private Paint mPaint = new Paint();
+    private Rect mRect = new Rect();
+    private Bitmap mBitmap;
 
     @Override
     public void onDraw(Canvas canvas){
@@ -35,6 +45,28 @@ public class Draw2D extends View {
 
         //draw green rectangle
         mPaint.setColor(Color.GREEN);
-        canvas.drawRect(20, 650, 1000, 800, mPaint);
+        canvas.drawRect(20, 850, 1000, 900, mPaint);
+
+        //draw text
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(48);
+        canvas.drawText(getResources().getString(R.string.printing_text), 20, 840, mPaint);
+
+        //draw angle text
+        int x = 550;
+        int y = 470;
+        mPaint.setColor(Color.GRAY);
+        mPaint.setTextSize(48);
+        String str2Rotate = getResources().getString(R.string.rotate_text);
+
+        canvas.rotate(-35, x + mRect.exactCenterX(), y + mRect.exactCenterY());
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawText(str2Rotate, x, y, mPaint);
+
+        canvas.restore();
+
+        canvas.drawBitmap(mBitmap, 400, 550, mPaint);
     }
 }
