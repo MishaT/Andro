1bf10de91844db4a523b138b7207aeb1567bbe07
Misha <misha_t@ua.fm>
2016-11-06 19:59:55 +0200

    added clock

--- play.with.android/day.17/17.Animation/app/src/main/java/com/example/misha/a17animation/MainActivity.java
+++ play.with.android/day.17/17.Animation/app/src/main/java/com/example/misha/a17animation/MainActivity.java
@@ -13,14 +13,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get the link to the Sun
+        //get link to the Sun
         ImageView sunImageView = (ImageView)findViewById(R.id.sun);
 
         //animation for the Sunrise
         Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
-
         //connect the animation
         sunImageView.startAnimation(sunRiseAnimation);
+
+        //get link to the clock
+        ImageView clockImageView = (ImageView)findViewById(R.id.clock);
+
+        //rotate the clock
+        Animation clockAnimation = AnimationUtils.loadAnimation(this, R.anim.clock_move);
+        clockImageView.startAnimation(clockAnimation);
 
     }
 }
