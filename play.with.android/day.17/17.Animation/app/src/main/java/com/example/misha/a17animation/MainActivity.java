b8233ade1b01bb158631ac3fdfc39010c29b4fa6
Misha <misha_t@ua.fm>
2016-11-06 18:41:42 +0200

    sun rise

--- play.with.android/day.17/17.Animation/app/src/main/java/com/example/misha/a17animation/MainActivity.java
+++ play.with.android/day.17/17.Animation/app/src/main/java/com/example/misha/a17animation/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.misha.a17animation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get the link to the Sun
+        ImageView sunImageView = (ImageView)findViewById(R.id.sun);
+
+        //animation for the Sunrise
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+
+        //connect the animation
+        sunImageView.startAnimation(sunRiseAnimation);
+
     }
 }
