7cadfeb62981ad0d74d1f4b78d4ee1c8d5945118
Misha <misha_t@ua.fm>
2016-11-19 15:36:45 +0200

    browser with go back

--- play.with.android/day.18/18.Browser/app/src/main/java/com/example/misha/a18browser/MainActivity.java
+++ play.with.android/day.18/18.Browser/app/src/main/java/com/example/misha/a18browser/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.misha.a18browser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    private class localWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (myWebView.canGoBack()){
+            myWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myWebView = (WebView)findViewById(R.id.webView);
+        myWebView.setWebViewClient(new localWebViewClient());
+
+        //turn JavaScript on
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        //set page to open
+        myWebView.loadUrl(getResources().getString(R.string.first_url));
     }
 }
