7b8afda84fbb58d482b05bd5c50a68af279825b8
Misha <misha_t@ua.fm>
2017-01-09 23:50:45 +0200

    Browser runner

---
+++ play.with.android/day.18/18.BrowserRunner/app/src/main/java/com/example/misha/a18browserrunner/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.misha.a18browserrunner;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void theButtonClick(View v){
+        Intent intent = new Intent("ua.ho.tolkachov.Browser");
+        intent.setData(Uri.parse(getResources().getString(R.string.start_page)));
+        startActivity(intent);
+    }
+}
