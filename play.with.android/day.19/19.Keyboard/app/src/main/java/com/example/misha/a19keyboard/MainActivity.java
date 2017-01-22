be97ab8e81ca39ee4db8ccc7ee92156f47eefbf8
Misha <misha_t@ua.fm>
2017-01-22 14:02:49 +0200

    simple back pressed

--- play.with.android/day.19/19.Keyboard/app/src/main/java/com/example/misha/a19keyboard/MainActivity.java
+++ play.with.android/day.19/19.Keyboard/app/src/main/java/com/example/misha/a19keyboard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.misha.a19keyboard;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed(){
+        showQuitDialog();
+        //super.onBackPressed();
+    }
+
+    private void showQuitDialog(){
+        AlertDialog.Builder exitDialog = new AlertDialog.Builder(MainActivity.this);
+        exitDialog.setTitle(getResources().getString(R.string.exit_dialog_title));
+
+        exitDialog.setPositiveButton(getResources().getString(R.string.exit_dialog_Yes),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+        exitDialog.setNegativeButton(getResources().getString(R.string.exit_dialog_No),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //do nothing
+                    }
+                });
+
+        exitDialog.show();
+
     }
 }
