8b8796392ef0cd7750b23aa3e875f9caf09cd6fe
Misha <misha_t@ua.fm>
2017-01-22 17:46:47 +0200

    short and long press

--- play.with.android/day.19/19.Keyboard/app/src/main/java/com/example/misha/a19keyboard/MainActivity.java
+++ play.with.android/day.19/19.Keyboard/app/src/main/java/com/example/misha/a19keyboard/MainActivity.java
@@ -5,6 +5,8 @@ import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +30,50 @@ public class MainActivity extends AppCompatActivity {
                             getResources().getText(R.string.double_press_to_exit),
                             Toast.LENGTH_SHORT).show();
         }
-
         back_pressed_time = System.currentTimeMillis();
+    }
 
+    @Override
+    protected void onUserLeaveHint(){
+        Toast.makeText(getApplicationContext(), "Home btn pressed", Toast.LENGTH_SHORT).show();
+        super.onUserLeaveHint();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        TextView editText = (TextView)findViewById(R.id.editText);
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK: {
+                editText.setText("Exiting...");
+                onBackPressed();
+                return true;
+            }
+            case KeyEvent.KEYCODE_VOLUME_UP: {
+                editText.setText("Short press");
+                event.startTracking();
+                return true;
+            }
+            case KeyEvent.KEYCODE_VOLUME_DOWN: {
+                editText.setText("Volume Down");
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event){
+        TextView editText = (TextView)findViewById(R.id.editText);
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+           // editText.setText("Loooong press!!");
+
+            Toast.makeText(getBaseContext(),
+                    "Loooong press!!",
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onKeyLongPress(keyCode, event);
     }
 
     private void showQuitDialog(){
